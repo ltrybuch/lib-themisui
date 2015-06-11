9072b628a96fd8e953162975846db87bcea06499
Sean Healy <s@xib.ca>
2015-06-11 10:07:21 -0600

    clio-21019 clarified polyfill syntax

--- polyfills/Element.matches.polyfill.js
+++ polyfills/Element.matches.polyfill.js
@@ -1,15 +1,19 @@
 this.Element && function(ElementPrototype) {
 	ElementPrototype.matches = ElementPrototype.matches ||
-	  ElementPrototype.matchesSelector ||
+		ElementPrototype.matchesSelector ||
 		ElementPrototype.mozMatchesSelector ||
 		ElementPrototype.msMatchesSelector ||
 		ElementPrototype.oMatchesSelector ||
 		ElementPrototype.webkitMatchesSelector ||
 		function (selector) {
-			var node = this, nodes = (node.parentNode || node.document).querySelectorAll(selector), i = -1;
+			var element = this;
+			var matches = (element.document || element.ownerDocument).querySelectorAll(selector);
+			var i = 0;
 
-			while (nodes[++i] && nodes[i] != node);
+			while (matches[i] && matches[i] !== element) {
+				i++;
+			}
 
-			return !!nodes[i];
+			return matches[i] ? true : false;
 		}
 }(Element.prototype);
