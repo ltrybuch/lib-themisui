77810abd1978cebafd90aee9e1c055134218602b
jefcurtis <jefcurtis@gmail.com>
2015-12-07 12:39:31 -0800

    clio-26874 Remove

--- polyfills/jQuery.typeAttribute.patch.js
+++ polyfills/jQuery.typeAttribute.patch.js
@@ -4,31 +4,26 @@
 // https://github.com/jquery/jquery/commit/aad235b3251494afe71fd5bb6031e11965af9bdb
 
 if(typeof jQuery == 'function' || typeof $ == 'function') {
-  console.log("is a function")
-  $(document).ready(function() {
-    console.log("is ready")
-    if (/1\.(7|8)\.\d/.test($.fn.jquery)) {
-      console.log("is 1.7.1")
-      jQuery.extend({
-        attrHooks: {
-          type: {
-            set: function( elem, value ) {
-                // Setting the type on a radio button after the value resets the value in IE6-9
-                // Reset value to it's default in case type is set after value
-                // This is for element creation
-              if ( !jQuery.support.radioValue && value === "radio" && jQuery.nodeName(elem, "input") ) {
-              // Reset value to default in case type is set after value during creation
-                var val = elem.value;
-                elem.setAttribute( "type", value );
-                if ( val ) {
-                  elem.value = val;
-                }
-                return value;
+  if (/1\.(7|8)\.\d/.test($.fn.jquery)) {
+    jQuery.extend({
+      attrHooks: {
+        type: {
+          set: function( elem, value ) {
+              // Setting the type on a radio button after the value resets the value in IE6-9
+              // Reset value to it's default in case type is set after value
+              // This is for element creation
+            if ( !jQuery.support.radioValue && value === "radio" && jQuery.nodeName(elem, "input") ) {
+            // Reset value to default in case type is set after value during creation
+              var val = elem.value;
+              elem.setAttribute( "type", value );
+              if ( val ) {
+                elem.value = val;
               }
+              return value;
             }
           }
         }
-      });
-    }
-  })
+      }
+    });
+  }
 }
